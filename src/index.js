80600176f2ae17df76eed271038d89bb83614569
Elias Rhouzlane <elias.rhouzlane@protonmail.com>
2022-10-05 16:44:11 +0200

    fix: dont purge if not in hasura

--- src/index.js
+++ src/index.js
@@ -7,29 +7,32 @@ export const clean = (sql) => async () =>
     .begin((sql) =>
       [
         sql`
-        DO $$
-        DECLARE
-          obj record;
-        BEGIN
-          DROP EVENT TRIGGER IF EXISTS "cacheinvalid__create_notify_table" RESTRICT;
-          DROP PROCEDURE IF EXISTS "cacheinvalid__create_notify" RESTRICT;
-          DROP FUNCTION IF EXISTS "cacheinvalid__create_notify_table" RESTRICT;
-          FOR obj IN
+          DO $$
+          DECLARE
+            obj record;
+          BEGIN
+            DROP EVENT TRIGGER IF EXISTS "cacheinvalid__create_notify_table" RESTRICT;
+            DROP PROCEDURE IF EXISTS "cacheinvalid__create_notify" RESTRICT;
+            DROP FUNCTION IF EXISTS "cacheinvalid__create_notify_table" RESTRICT;
+            FOR obj IN
             SELECT
               table_name,
-              format('cacheinvalid__notify_on_%s', x.op) as trigger_name
+              format('cacheinvalid__notify_on_%s', x.op) AS trigger_name
             FROM
-              information_schema.tables, (VALUES ('insert'), ('delete'), ('update')) as x(op)
+              information_schema.tables,
+              (
+                VALUES ('insert'),
+                  ('delete'),
+                  ('update')) AS x (op)
             WHERE
               table_schema = 'public'
-              AND table_type = 'BASE TABLE'
-          LOOP
-            EXECUTE format('DROP TRIGGER IF EXISTS %s ON %s', obj.trigger_name, obj.table_name);
-          END LOOP;
-          DROP FUNCTION IF EXISTS "cacheinvalid__notify" RESTRICT;
-        END;
-        $$
-      `,
+              AND table_type = 'BASE TABLE' LOOP
+                EXECUTE format('DROP TRIGGER IF EXISTS %s ON %s', obj.trigger_name, obj.table_name);
+              END LOOP;
+            DROP FUNCTION IF EXISTS "cacheinvalid__notify" RESTRICT;
+          END;
+          $$
+        `,
       ].map(handleDebug.map)
     )
     .then(handleDebug.begin);
@@ -40,46 +43,53 @@ export default (sql) =>
       .begin((sql) =>
         [
           sql`
-          CREATE OR REPLACE FUNCTION "public"."cacheinvalid__notify" ()
-            RETURNS TRIGGER
-            AS $$
-          DECLARE
-            o_table_name text := TG_TABLE_NAME::text;
-            op text := TG_OP::text;
-            obj record;
-            need_join boolean := TG_ARGV[0]::boolean;
-          BEGIN
-            FOR obj IN
-            SELECT
-              format('SELECT pg_notify(''cacheinvalid__notify'', jsonb_build_object(''op'', %L, ''table'', %L, ''views'', %L::jsonb, ''key_fields'', jsonb_agg(jsonb_build_object(%s)))::varchar(7999)) FROM %s HAVING count(sq.*) > 0', op, o_table_name, array_to_json(ARRAY (
-                    SELECT
-                      u.view_name FROM information_schema.view_table_usage u
-                    WHERE
-                      u.table_schema NOT IN ('information_schema', 'pg_catalog')
-                    AND u.table_name = o_table_name
-                    AND (SELECT jsonb_path_query_array(metadata::jsonb, '$.sources[*].tables[*].table.name') FROM "hdb_catalog".hdb_metadata) ? u.view_name)), string_agg(format('''%1$s'', %1$s', kcu.column_name), ', '), CASE WHEN need_join THEN
+            CREATE OR REPLACE FUNCTION "public"."cacheinvalid__notify" ()
+              RETURNS TRIGGER
+              AS $$
+            DECLARE
+              o_table_name text := TG_TABLE_NAME::text;
+              op text := TG_OP::text;
+              obj record;
+              need_join boolean := TG_ARGV[0]::boolean;
+            BEGIN
+              FOR obj IN
+              SELECT
+                format('SELECT pg_notify(''cacheinvalid__notify'', jsonb_build_object(''op'', %L, ''table'', %L, ''purge'', %L::jsonb, ''key_fields'', jsonb_agg(jsonb_build_object(%s)))::varchar(7999)) FROM %s HAVING count(sq.*) > 0', op, o_table_name, array_to_json(ARRAY (
+                      SELECT
+                        sq.name FROM (
+                          SELECT
+                            o_table_name AS name
+                          UNION
+                          SELECT
+                            u.view_name AS name FROM information_schema.view_table_usage u
+                          WHERE
+                            u.table_schema NOT IN ('information_schema', 'pg_catalog')
+                          AND u.table_name = o_table_name) sq
+                    WHERE (
+                      SELECT
+                        jsonb_path_query_array(metadata::jsonb, '$.sources[*].tables[*].table.name')
+                      FROM "hdb_catalog".hdb_metadata) ? sq.name)), string_agg(format('''%1$s'', %1$s', kcu.column_name), ', '), CASE WHEN need_join THEN
                   '(SELECT * FROM ref_table2 UNION SELECT * FROM ref_table) sq'
                 ELSE
                   'ref_table sq'
                 END) AS x
-            FROM
-              information_schema.key_column_usage kcu
-              JOIN information_schema.table_constraints tc ON tc.constraint_name = kcu.constraint_name
-              JOIN information_schema.columns c ON c.column_name = kcu.column_name
-                AND c.table_name = kcu.table_name
-            WHERE
-              kcu.table_schema = 'public'
-              AND kcu.table_name = o_table_name
-              AND tc.constraint_type = 'PRIMARY KEY'
-              LOOP
-                EXECUTE obj.x;
-              END LOOP;
-            RETURN NEW;
-          END;
-          $$
-          LANGUAGE plpgsql;
-          
-        `,
+              FROM
+                information_schema.key_column_usage kcu
+                JOIN information_schema.table_constraints tc ON tc.constraint_name = kcu.constraint_name
+                JOIN information_schema.columns c ON c.column_name = kcu.column_name
+                  AND c.table_name = kcu.table_name
+              WHERE
+                kcu.table_schema = 'public'
+                AND kcu.table_name = o_table_name
+                AND tc.constraint_type = 'PRIMARY KEY' LOOP
+                  EXECUTE obj.x;
+                END LOOP;
+              RETURN NEW;
+            END;
+            $$
+            LANGUAGE plpgsql;
+            
+          `,
           sql`    
           CREATE OR REPLACE PROCEDURE "public"."cacheinvalid__create_notify" (table_name text)
           AS $$
@@ -109,51 +119,50 @@ export default (sql) =>
           $$
           LANGUAGE plpgsql;`,
           sql`
-          DO $$
-          DECLARE
-            obj record;
-          BEGIN
-            FOR obj IN
-            SELECT
-              table_name
-            FROM
-              information_schema.tables
-            WHERE
-              table_schema = 'public'
-              AND table_type = 'BASE TABLE' LOOP
-                EXECUTE format('CALL cacheinvalid__create_notify(%L)', obj.table_name);
-              END LOOP;
-          END
-          $$;
-          
-        `,
+            DO $$
+            DECLARE
+              obj record;
+            BEGIN
+              FOR obj IN
+              SELECT
+                table_name
+              FROM
+                information_schema.tables
+              WHERE
+                table_schema = 'public'
+                AND table_type = 'BASE TABLE' LOOP
+                  EXECUTE format('CALL cacheinvalid__create_notify(%L)', obj.table_name);
+                END LOOP;
+            END
+            $$;
+            
+          `,
           sql`
-          CREATE OR REPLACE FUNCTION "public"."cacheinvalid__create_notify_table" ()
-            RETURNS event_trigger
-            AS $$
-          DECLARE
-            obj record;
-            table_name text;
-          BEGIN
-            FOR obj IN
-            SELECT
-              *
-            FROM
-              pg_event_trigger_ddl_commands ()
-              LOOP
-                table_name := split_part(obj.object_identity, '.', 2);
-                PERFORM
-                  pg_notify('cacheinvalid__notify', jsonb_pretty(jsonb_build_object('table', table_name)));
-                IF obj.command_tag IN ('CREATE TABLE', 'CREATE TABLE AS') THEN
-                  CALL
-                    cacheinvalid__create_notify (table_name);
-                END IF;
-              END LOOP;
-          END;
-          $$
-          LANGUAGE plpgsql;
-          
-        `,
+            CREATE OR REPLACE FUNCTION "public"."cacheinvalid__create_notify_table" ()
+              RETURNS event_trigger
+              AS $$
+            DECLARE
+              obj record;
+              table_name text;
+            BEGIN
+              FOR obj IN
+              SELECT
+                *
+              FROM
+                pg_event_trigger_ddl_commands ()
+                LOOP
+                  table_name := split_part(obj.object_identity, '.', 2);
+                  PERFORM
+                    pg_notify('cacheinvalid__notify', jsonb_pretty(jsonb_build_object('table', table_name)));
+                  IF obj.command_tag IN ('CREATE TABLE', 'CREATE TABLE AS') THEN
+                    CALL cacheinvalid__create_notify (table_name);
+                  END IF;
+                END LOOP;
+            END;
+            $$
+            LANGUAGE plpgsql;
+            
+          `,
           sql`
         CREATE EVENT TRIGGER "cacheinvalid__create_notify_table" ON ddl_command_end
             WHEN TAG IN (
--- src/stellate.connector.js
+++ src/stellate.connector.js
@@ -1,10 +1,10 @@
-import { handleDebug, pipe, throwErr } from "./utils.js";
+import { handleDebug, noopLog, pipe, throwErr } from "./utils.js";
 import { fetch } from "undici";
 
 export default ({ url, token, soft }) =>
   pipe(
-    ({ key_fields, table, views, all }) => ({
-      views,
+    ({ key_fields, table, purge, all }) => ({
+      purge,
       table,
       all,
       soft: Boolean(soft),
@@ -18,20 +18,26 @@ export default ({ url, token, soft }) =>
         ?.flat()
         ?.filter((v) => v),
     }),
-    ({ table, values, views, all, soft }) => [
-      !all && values?.length > 0 && values?.[0]?.name !== "id"
-        ? `_purgeType(soft: ${soft}, type: ${JSON.stringify(
-            table
-          )}, keyFields: ${JSON.stringify(values)
-            .replace(/"name":/g, "name:")
-            .replace(/"value":/g, "value:")})`
-        : !all && values?.length > 0
-        ? `purge${table}(soft: ${soft}, id: ${JSON.stringify(
-            values.map(({ value }) => value)
-          )})`
-        : `purge${table}(soft: ${soft})`,
-      views?.map((view_name) => `purge${view_name}(soft: ${soft})`),
-    ],
+    ({ table, values, purge, all, soft }) =>
+      [
+        purge.includes(table) &&
+          (!all && values?.length > 0 && values?.[0]?.name !== "id"
+            ? `_purgeType(soft: ${soft}, type: ${JSON.stringify(
+                table
+              )}, keyFields: ${JSON.stringify(values)
+                .replace(/"name":/g, "name:")
+                .replace(/"value":/g, "value:")})`
+            : !all && values?.length > 0
+            ? `purge${table}(soft: ${soft}, id: ${JSON.stringify(
+                values.map(({ value }) => value)
+              )})`
+            : `purge${table}(soft: ${soft})`),
+        ...purge
+          ?.filter((name) => name !== table)
+          ?.map((view_name) => `purge${view_name}(soft: ${soft})`),
+      ].filter((v) => v),
+    (arr) => arr.join("\n"),
+    handleDebug.noopLog,
     (string) =>
       fetch(url, {
         method: "POST",
